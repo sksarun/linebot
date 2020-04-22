ab586a6e85adaeda118bf2d1e352cf7657129115
Sarun <sksarun.k@gmail.com>
2020-04-22 22:55:35 +0700

    add test

--- SimpleBot.py
+++ SimpleBot.py
@@ -34,7 +34,7 @@ def webhook():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=event.message.text+'new'))
 
 
 if __name__ == "__main__":
