c4fee4354e65558a2ef65b09f44ee24ee93a9e79
Sarun <sksarun.k@gmail.com>
2020-05-24 15:37:56 +0700

    arrau should be clean

--- Factory/PeriodsFactory.py
+++ Factory/PeriodsFactory.py
@@ -5,6 +5,7 @@ class PeriodsFactory:
     periods = []
     
     def __init__ (self):
+        self.periods = []
         self.periods.append(Period(1,22,26,0,0)) 
         self.periods.append(Period(2,12,15,0,0))
         self.periods.append(Period(3,18,22,0,0))
