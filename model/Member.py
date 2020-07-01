03f66c7540ca97ed7bee251ff39a01a25cd97097
sksarun <sksarun.k@gmail.com>
2020-07-01 02:55:34 +0000

    add new files

---
+++ model/Member.py
@@ -0,0 +1,7 @@
+class Member:
+    name = ''
+    team = ''
+    def __init__(self,name,team):
+        self.name = name 
+        self.team = team
+    
---
+++ model/Task.py
@@ -0,0 +1,5 @@
+class Task:
+    desc = ''
+    def __init__(self,desc):
+        self.desc = desc
+        
